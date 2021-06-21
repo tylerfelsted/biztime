909473b7abaa844dd4a0257c46b3d20a5e71d50f
Tyler <tyler.felsted@yahoo.com>
2021-06-21 16:12:56 -0400

    Connected to db and exported client

--- db.js
+++ db.js
@@ -1,2 +1,16 @@
 /** Database setup for BizTime. */
+const { Client } = require("pg");
 
+let DB_URI;
+
+if(process.env.NODE_ENV === 'test') {
+    DB_URI = "postgresql://postgres:postgres@localhost:5433/biztime_test";
+} else {
+    DB_URI = "postgresql://postgres:postgres@localhost:5433/biztime";
+}
+
+let db = new Client({ connectionString: DB_URI});
+db.connect();
+
+
+module.exports = db;
